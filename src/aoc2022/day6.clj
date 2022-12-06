9d70f0c6102cadbc8483e6390ac8290a7c8cbd84
Cian Synnott <cian@emauton.org>
2022-12-06 23:28:50 +0000

    AoC 2022 day 6: name and document the iteration fn
    This was a bit obscure all in one block; it should be easier to
    understand now.

--- src/aoc2022/day6.clj
+++ src/aoc2022/day6.clj
@@ -1,18 +1,24 @@
 (ns aoc2022.day6
   (:require [aoc2022.util :as util]))
 
+(defn next-marker-window
+  "Given a triple of [letters stream index], chop off the first `n` letters of
+  stream to use as the next set, and shift our 'window' on the stream forward
+  by 1 letter"
+  [n [_, stream, index]]
+  [(set (take n stream)) (rest stream) (inc index)])
+
+
 (defn detect-marker
-  "return the index directly after the first marker in stream: count-distinct different
+  "Return the index directly after the first marker in stream: count-distinct different
    characters"
   [stream count-distinct]
   ; construct a lazy sequence of count-distinct prefixes + some accounting
   ; then filter for the first sequence where count-distinct are different
-  (let [sets (iterate (fn [[_ remainder n]]
-                        (let [buffer (set (take count-distinct remainder))]
-                          [buffer (rest remainder) (inc n)]))
+  (let [sets (iterate (partial next-marker-window count-distinct)
                       [(set '()) stream 0])
-        [_ _ index] (first (filter (fn [[segment _ _]]
-                                     (= count-distinct (count segment)))
+        [_ _ index] (first (filter (fn [[letters _ _]]
+                                     (= count-distinct (count letters)))
                                    sets))]
     (+ index (dec count-distinct))))
 
